c35dff779c8fb84006b0fe3be54822540a746efa
Leif Gensert <leif@propertybase.com>
2013-02-19 11:13:09 +0100

    bump version

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer_email 'leif@propertybase.com'
 license          'MIT'
 description      'Checkpoint End-Point Ubuntu'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
